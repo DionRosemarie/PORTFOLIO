4c9805ee01499f4994c095973913e0a67d76bbe0
Rose-Marie Dion <dion.rosemarie@hotmail.com>
2019-01-24 23:22:16 -0500

    DEBUTING

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+
+"use strict";
+
+$(document).ready(setup);
+
+function setup() {
+  let $divs = $('div');
+  $divs.hide();
+  $divs.fadeIn(2000);
+}
