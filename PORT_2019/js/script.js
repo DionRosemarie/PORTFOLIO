9c560917ab5664b5cee6bfb0ae0875dea412b182
Rose-Marie Dion <dion.rosemarie@hotmail.com>
2019-01-25 10:05:50 -0500

    trying to put the video online

--- PORT_2019/js/script.js
+++ PORT_2019/js/script.js
@@ -7,14 +7,3 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
-
-
-"use strict";
-
-$(document).ready(setup);
-
-function setup() {
-  let $divs = $('div');
-  $divs.hide();
-  $divs.fadeIn(2000);
-}
